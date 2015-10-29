9e9627a13a3496a191ab5e3e6e3c66257699694f
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2015-10-29 18:53:14 +0900

    New: set output dir

--- shell/exe.sh
+++ shell/exe.sh
@@ -2,17 +2,26 @@
 
 set -ue
 
+if [ $# -eq 1 ]; then
+  outputdir=$1
+elif [ $# -eq 0 ]; then
+  outputdir=.
+else
+  echo "argument error"
+  exit 1
+fi
+
 function juman_wakati(){
   cat - | juman | grep -v "^@" | awk '{print $1}' | tr '\n' ' ' | sed -e 's/ EOS /\n/g'
 }
 
-if [ ! -e data/orig.txt ]; then
-  echo "Not found: data/orig.txt"
+if [ ! -e $outputdir/data/orig.txt ]; then
+  echo "Not found: $outputdir/data/orig.txt"
   exit 1
 fi
 
-if [ ! -e data/changed.txt ]; then
-  echo "Not found: data/changed.txt"
+if [ ! -e $outputdir/data/changed.txt ]; then
+  echo "Not found: $outputdir/data/changed.txt"
   exit 1
 fi
 
@@ -23,20 +32,19 @@ if [ ! -e model10000k/model ]; then
 fi
 
 #分かち書きの後、<unk>を置換してテストデータ作成
-files=("data/orig.txt" "data/changed.txt")
+files=("$outputdir/data/orig.txt" "$outputdir/data/changed.txt")
 for f in $files; do
-  # lv $f | juman_wakati > jumanwakati/$f:t:r".jumanwakati"
-   lv $f | juman_wakati | python src/make_test_data.py  > testdata/$f:t:r".jumanwakati"
+   lv $f | juman_wakati | python src/make_test_data.py  > $outputdir/testdata/$f:t:r".jumanwakati"
 done
 
 #RNNLMでテスト
-testfiles=("testdata/orig.jumanwakati" "testdata/changed.jumanwakati")
+testfiles=("$outputdir/testdata/orig.jumanwakati" "$outputdir/testdata/changed.jumanwakati")
 for f in $testfiles; do
   shell/test.sh $f
 done
 
 #結果を1つのファイルに結合
-paste -d '\t' result/orig.result result/changed.result| awk -F'\t' '{print $0"\t"$4/$2}' > result/result.txt
+paste -d '\t' $outputdir/result/orig.result $outputdir/result/changed.result| awk -F'\t' '{print $0"\t"$4/$2}' > $outputdir/result/result.txt
 
 #結果を整形
-./shell/show_result.sh result/result.txt > result/result.format.txt
+./shell/show_result.sh $outputdir/result/result.txt > $outputdir/result/result.format.txt
--- shell/setup.sh
+++ shell/setup.sh
@@ -1,3 +1,16 @@
 #!/bin/zsh
 
 cat model10000k/model_splitted_* > model10000k/model
+
+if [ $# -eq 1 ]; then
+  outputdir=$1
+  mkdir $outputdir
+  mkdir $outputdir/data
+  mkdir $outputdir/result
+  mkdir $outputdir/testdata
+elif [ $# -eq 0 ]; then
+  #Do nothing
+else
+  echo "argument error"
+  exit 1
+fi
